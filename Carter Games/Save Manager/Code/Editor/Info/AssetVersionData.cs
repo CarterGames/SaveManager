634fe14a9318f5ca88916c2bc22b5d9c523b1068
Jonathan <jonathan@carter.games>
2025-01-27 08:00:32 +0000

    🔩 Version release date update

--- Carter Games/Save Manager/Code/Editor/Info/AssetVersionData.cs
+++ Carter Games/Save Manager/Code/Editor/Info/AssetVersionData.cs
@@ -40,6 +40,6 @@ namespace CarterGames.Assets.SaveManager.Editor
         /// <remarks>
         /// Asset owner is in the UK, so its D/M/Y format.
         /// </remarks>
-        public static string ReleaseDate => "2025/01/26";
+        public static string ReleaseDate => "2025/01/27";
     }
 }
